22ec2168d9996cabf56344f333a5951e9d34d5b3
Flopiras <Flopiras@yahoo.it>
2023-11-17 12:57:00 +0100

    added list

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,5 +1,36 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+	public static void main(String[] args) {
+
+		List<String> gifts = new ArrayList<>();
+
+		Boolean hasFinish = false;
+
+		Scanner in = new Scanner(System.in);
+
+		while (!hasFinish) {
+
+			System.out.println("Vuoi aggiungere un regalo alla lista?");
+			String strAnswer = in.nextLine();
+			if (strAnswer.equals("no")) {
+				hasFinish = true;
+				break;
+			}
+			
+			System.out.println("Che regalo vuoi aggiungere?");
+			String gift = in.nextLine();
+			gifts.add(gift);
+		}
+		
+		in.close();
+		
+		System.out.println("Ci sono " + gifts.size() + " regali nella lista!");
+	}
 
 }
